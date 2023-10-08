e05f5057fdaf68a9f4126c6e619fd51cbce19d21
param-code <sparamveer1001@gmail.com>
2023-10-08 09:30:20 +0530

    many

---
+++ Break&Continue.py
@@ -0,0 +1,10 @@
+i = 1
+while(True):
+    if i < 10:
+        i += 1
+        continue
+    else:print(i , end = " ")
+    if i > 45:
+        i += 1
+        break
+    i += 1
---
+++ Quiz_Loop.py
@@ -0,0 +1,5 @@
+while(True):
+    n = int(input("Enter your Number"))
+    if n > 100:
+        print("Congrats! You have enter a number greater than 100")
+        break
