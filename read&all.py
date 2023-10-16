23be4fec402a2d6d736f34236536ab8db34ac540
param-code <sparamveer1001@gmail.com>
2023-10-16 07:42:29 +0530

    write&append

--- read&all.py
+++ read&all.py
@@ -2,4 +2,4 @@ f = open("read&all.txt","r")
 # content = f.read()
 for line in f:
     print(line)
-f.close() 
+f.close()
---
+++ write&append.py
@@ -0,0 +1,4 @@
+f = open("hello.txt","a")
+no_of_characters = f.write("Harry is a Good boy\n")
+print(no_of_characters)
+f.close()
