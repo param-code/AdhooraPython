5148dd7ec384defca27c8b1ce94a0527710d0603
param-code <sparamveer1001@gmail.com>
2023-10-21 07:56:36 +0530

    Pytube

---
+++ Pytubee.py
@@ -0,0 +1,7 @@
+# from pytube import Playlist
+# Play_list = Playlist("https://www.youtube.com/playlist?list=PLu0W_9lII9agICnT8t4iYVSZ3eykIAOME")
+# print(len(Play_list))
+# for link in Play_list:
+#     print(link)
+# for video in Play_list.videos:
+#     print(video.title)
