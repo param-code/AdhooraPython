b54b60b3e012389a822df9646cf7a8962329c6b5
param-code <sparamveer1001@gmail.com>
2023-10-24 20:26:42 +0530

    ptube

--- Pytubee.py
+++ Pytubee.py
@@ -5,3 +5,21 @@
 #     print(link)
 # for video in Play_list.videos:
 #     print(video.title)
+# from pytube import YouTube
+
+# def Download(link):
+#     youtubeObject = YouTube(link)
+#     youtubeObject = youtubeObject.streams.get_highest_resolution()
+#     try:
+#         youtubeObject.download()
+#     except:
+#         print("An error has occurred")
+#     print("Download is completed successfully")
+
+
+# link = input("Enter the YouTube video URL: ")
+# Download(link)
+# from pytube import Playlist
+# Play_list = Playlist("https://youtube.com/playlist?list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz&feature=shared")
+# stream = Play_list.get_highest_resolution()
+# stream.download()
